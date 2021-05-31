34628d20f10dba8d6e51002e5ef3935d71fc93dd
Iris Ji <45611041+IrisJi3@users.noreply.github.com>
2021-05-30 21:28:35 -0400

    Update groceries.js

--- Lab2/scripts/groceries.js
+++ Lab2/scripts/groceries.js
@@ -30,10 +30,10 @@ var products = [
 function restrictListProducts(prods, restriction) {
 	let product_names = [];
 	for (let i=0; i<prods.length; i+=1) {
-		if ((restriction == "Lactose-intolerant") && (prods[i].lactose_intolerant == true)){
+		if ((restriction == "Lactose-intolerant") && (prods[i].lactose_intolerant == false)){
 			product_names.push(prods[i].name);
 		}
-		else if ((restriction == "Nuts-allergic") && (prods[i].nuts_allergic == true)){
+		else if ((restriction == "Nuts-allergic") && (prods[i].nuts_allergic == false)){
 			product_names.push(prods[i].name);
 		}
 		else if (restriction == "None"){
